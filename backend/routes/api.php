317c379f1e623a097560309f56be74802792939e
Webartdp <37107616+Webartdp@users.noreply.github.com>
2026-07-06 11:03:28 +0300

    Add work events list API route

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function () {
     Route::get('/employee/field-state', EmployeeFieldStateController::class);
+    Route::get('/work-events', [WorkEventController::class, 'index']);
 
     Route::post('/work-events/gasfahrt/start', [WorkEventController::class, 'startGasfahrt']);
     Route::post('/work-events/gasfahrt/stop', [WorkEventController::class, 'stopGasfahrt']);
