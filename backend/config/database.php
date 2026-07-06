237ba35ac979a6d681ebe3f6ee23ee5706d744fa
Webartdp <37107616+Webartdp@users.noreply.github.com>
2026-07-06 10:57:21 +0300

    Add Laravel database config

---
+++ backend/config/database.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    'default' => env('DB_CONNECTION', 'pgsql'),
+    'connections' => [
+        'pgsql' => [
+            'driver' => 'pgsql',
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '5432'),
+            'database' => env('DB_DATABASE', 'rail_crm'),
+            'username' => env('DB_USERNAME', 'rail_crm'),
+            'password' => env('DB_PASSWORD', ''),
+            'charset' => 'utf8',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'search_path' => 'public',
+            'sslmode' => 'prefer',
+        ],
+    ],
+    'migrations' => 'migrations',
+];
